0e1f5b996a5f3515cff27683fefef8adeacca6f1
pleonardy <p.leonardy@sig.eu>
2018-05-29 09:58:55 +0200

    Introducing duplication

--- code/src/HelloWorld.java
+++ code/src/HelloWorld.java
@@ -3,6 +3,23 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("How are you doing?");
+		System.out.println("Happy to see you again");
+		System.out.println("Sunny day today");
+		System.out.println("Checking for duplicates");
+		System.out.println("Yet another line");
+		System.out.println("And another one");
+		System.out.println("Going to 8 lines, should be enough to trigger duplication violation");
+		
+		System.out.println("Hello world!");
+		System.out.println("How are you doing?");
+		System.out.println("Happy to see you again");
+		System.out.println("Sunny day today");
+		System.out.println("Checking for duplicates");
+		System.out.println("Yet another line");
+		System.out.println("And another one");
+		System.out.println("Going to 8 lines, should be enough to trigger duplication violation");
+
 
 	}
 
